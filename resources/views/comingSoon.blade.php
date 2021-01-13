a226f05f4eca01dcf3d66dcd206397ceadc4af26
willian jesus rodriguez ramirez <rodriguezwillian95@gmail.com>
2021-01-13 13:54:14 -0400

    first commit

---
+++ resources/views/comingSoon.blade.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="{{ url('comingSoonAssets/css/style.css') }}">
+    <link href="https://fonts.googleapis.com/css2?family=Lato&display=swap" rel="stylesheet">
+    <link  rel="icon"   href="{{ url('comingSoonAssets/img/favicon.png') }}" type="image/png" />
+    <title>Wikipbl</title>
+</head>
+
+<body class="">
+    <div class="div-coming">
+        <div class="coming-soon">
+            <div>
+                <img class="logo" src="{{ url('comingSoonAssets/img/wiki.svg') }}" alt="logo wikipbl">
+            </div>
+            <div>
+                <h1>COMING SOON</h1>
+            </div>
+        </div>
+       <!----- <div class="social-div">
+            <div class="social">
+                <a href="https://www.facebook.com/aidacaceresart">
+                    <img class="icon-social" src="assets/img/facebook.svg" alt="Facebook">
+                    Aida Cáceres Art
+                </a>
+            </div>
+            <div class="social">
+                <a href="https://instagram.com/aidacaceresart?igshid=1b5fy53tvwzc5">
+                    <img class="icon-social" src="assets/img/instagram.svg" alt="Instagram">
+                    aidacaceresart
+                </a>
+            </div>
+        </div>--->
+    </div>
+</body>
+
+</html>
