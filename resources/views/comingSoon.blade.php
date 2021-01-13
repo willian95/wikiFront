4794cd3a3f1a193380148ac8ffcacdf1381b9536
willian jesus rodriguez ramirez <rodriguezwillian95@gmail.com>
2021-01-13 15:57:05 -0400

    cambios

--- resources/views/comingSoon.blade.php
+++ resources/views/comingSoon.blade.php
@@ -6,7 +6,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link rel="stylesheet" href="{{ url('comingSoonAssets/css/style.css') }}">
         <link href="https://fonts.googleapis.com/css2?family=Lato&display=swap" rel="stylesheet">
-        <link rel="icon" type="image/png" href="{{ url('comingSoonAssets/img/favicon.png') }}">
+        <link rel="shortcut icon" type="image/x-icon" hre="{{ url('comingSoonAssets/img/favicon.png') }}">
         <title>Wikipbl</title>
     </head>
 
