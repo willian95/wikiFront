d36f0c6d176656afa98a304e434183cb25cdcba1
willian jesus rodriguez ramirez <rodriguezwillian95@gmail.com>
2021-01-13 19:35:44 -0400

    cambios

--- resources/views/comingSoon.blade.php
+++ resources/views/comingSoon.blade.php
@@ -5,22 +5,24 @@
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link rel="stylesheet" href="{{ url('comingSoonAssets/css/style.css') }}">
-        <link href="https://fonts.googleapis.com/css2?family=Lato&display=swap" rel="stylesheet">
-        <link rel="shortcut icon" type="image/x-icon" hre="{{ url('comingSoonAssets/img/favicon.png') }}">
+        <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Inconsolata:wght@600&display=swap" rel="stylesheet">
+        <link rel="shortcut icon" type="image/x-icon" href="{{ url('comingSoonAssets/img/favicon.png') }}">
+    
         <title>Wikipbl</title>
     </head>
 
-<body>
-    <div class="div-coming">
-        <div class="coming-soon">
-            <div>
-                <img class="logo" src="{{ url('comingSoonAssets/img/logo.png') }}" alt="logo wikipbl">
-            </div>
-            <div>
-                <h1>COMING SOON</h1>
+    <body class="">
+        <div class="div-coming">
+            <div class="coming-soon">
+                <div>
+                    <img class="logo" src="{{ url('comingSoonAssets/img/logo.svg') }}" alt="logo wikipbl">
+                </div>
+                <div>
+                    <h1>COMING SOON</h1>
+                </div>
             </div>
         </div>
-    </div>
-</body>
+    </body>
 
 </html>
