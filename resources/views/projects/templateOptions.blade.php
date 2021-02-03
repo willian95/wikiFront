c7ef599f00c20dea28e327661ad1d071dc3de1f3
willian jesus rodriguez ramirez <rodriguezwillian95@gmail.com>
2021-02-03 02:45:53 -0400

    wiki template

--- resources/views/projects/templateOptions.blade.php
+++ resources/views/projects/templateOptions.blade.php
@@ -5,14 +5,14 @@
     <div class="row">
         <div class="col-md-6 flex-template ">
             <img class="mt-4 mb-4" alt='icno' src="{{ url('assets/img/iconos/template (1).svg') }}">
-            <button class="btn btn-custom mb-3">Use Wikipbl template</button>
+            <a href="{{ url('project/wikiPBL-template/create') }}" class="btn btn-custom mb-3">Use Wikipbl template</a>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae, maxime dolores consectetur perferendis
                 nobis recusandae quas libero similique est tempora, tenetur cupiditate? Aliquid reiciendis, molestiae
                 totam temporibus praesentium nihil exercitationem.</p>
         </div>
         <div class="col-md-6 flex-template ">
             <img class="mt-4 mb-4" alt='icono' src="{{ url('assets/img/iconos/template2.svg') }}">
-            <button class="btn btn-custom mb-3">Use my own template</button>
+            <a href="{{ url('project/own-template/create') }}" class="btn btn-custom mb-3">Use my own template</a>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae, maxime dolores consectetur perferendis
                 nobis recusandae quas libero similique est tempora, tenetur cupiditate? Aliquid reiciendis, molestiae
                 totam temporibus praesentium nihil exercitationem.</p>
