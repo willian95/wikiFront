eb0dc67e93b00e4ab7fda777930f89b4b710560c
patriciax <yively.pa@gmail.com>
2021-03-02 13:47:23 -0400

    wiki hast

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -42,10 +42,15 @@ $(function () {
 
 
   $('.dropdown-toggle').dropdown()
-
+ // Fix input element click problem
+ $('.dropdown input, .dropdown label').click(function(e) {
+  e.stopPropagation();
+});
   $('.collapse').collapse()
 
 });
+
+
 
 
 $(window).scroll(function() {
