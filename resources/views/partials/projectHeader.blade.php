1bfb0fd6244afc258d8124bde9e031d8fef9125b
willian jesus rodriguez ramirez <rodriguezwillian95@gmail.com>
2021-04-20 07:51:31 -0400

    cambios

--- resources/views/partials/projectHeader.blade.php
+++ resources/views/partials/projectHeader.blade.php
@@ -174,31 +174,15 @@
                                         </li>--->
 
                                         @endif
-                                        {{--<li class="nav-item   flex-main ">
-                                    <img alt='icon' class="login_icon "
-                                    src="{{ url('assets/img/iconos/group.svg') }}">
-                                        <!-- Rounded switch -->
-                                        <label class="switch">
-                                            <input type="checkbox">
-                                            <span class="slider slider-nav round"></span>
-                                        </label>
-                                        </li>--}}
-
-                                        {{--@if($action == "edit")
-                                            @if(\Auth::user()->id == $project[0]->user_id && $project[0]->is_private == 1)
-                                            <li class="nav-itm last-style mb-2 mr-4 " style="cursor: pointer;" @click="erase()"><svg class="login_icon color-blue_icon hover-trash" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
-                                                    <path d="M10,18a1,1,0,0,0,1-1V11a1,1,0,0,0-2,0v6A1,1,0,0,0,10,18ZM20,6H16V5a3,3,0,0,0-3-3H11A3,3,0,0,0,8,5V6H4A1,1,0,0,0,4,8H5V19a3,3,0,0,0,3,3h8a3,3,0,0,0,3-3V8h1a1,1,0,0,0,0-2ZM10,5a1,1,0,0,1,1-1h2a1,1,0,0,1,1,1V6H10Zm7,14a1,1,0,0,1-1,1H8a1,1,0,0,1-1-1V8H17Zm-3-1a1,1,0,0,0,1-1V11a1,1,0,0,0-2,0v6A1,1,0,0,0,14,18Z" />
-                                                </svg></li>
-                                            @endif
-                                        @endif--}}
+                            
 
                                         <li class="nav-item   flex-main ml-5" @if($projectAction=='creation' ) @click="saveProject()" @else @click="saveEditionProject()" @endif>
                                             <svg class="login_icon color-blue_icon " xmlns="http://www.w3.org/2000/svg" data-name="Layer 1" viewBox="0 0 24 24">
                                                 <path d="M20.71,9.29l-6-6a1,1,0,0,0-.32-.21A1.09,1.09,0,0,0,14,3H6A3,3,0,0,0,3,6V18a3,3,0,0,0,3,3H18a3,3,0,0,0,3-3V10A1,1,0,0,0,20.71,9.29ZM9,5h4V7H9Zm6,14H9V16a1,1,0,0,1,1-1h4a1,1,0,0,1,1,1Zm4-1a1,1,0,0,1-1,1H17V16a3,3,0,0,0-3-3H10a3,3,0,0,0-3,3v3H6a1,1,0,0,1-1-1V6A1,1,0,0,1,6,5H7V8A1,1,0,0,0,8,9h6a1,1,0,0,0,1-1V6.41l4,4Z" />
                                             </svg>
                                             <span class="tooltip-nav">Save wikiPBL</span>
-                                            <!-- <img alt='icon' class="login_icon "
-                                src="{{ url('assets/img/iconos/save.svg') }}">---->
+                                            <img alt='icon' class="login_icon "
+                                src="{{ url('assets/img/iconos/save.svg') }}">
                                         </li>
 
 
